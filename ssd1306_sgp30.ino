9c4a9de7df0e136d36c30d77102cf35f9805ef3f
Jan <janschill@proton.me>
2024-01-04 09:51:32 +0100

    Update ssd1306_sgp30.ino

--- ssd1306_sgp30.ino
+++ ssd1306_sgp30.ino
@@ -4,7 +4,6 @@
 2. Determine if air quality is good or bad
 3. Print
   3a. Support multi language
-  3b. 
 
 SGP30
 TVOC (Total Volatile Organic Compound)
@@ -19,61 +18,83 @@ eCO2 (equivalent calculated carbon-dioxide)
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "SparkFun_SGP30_Arduino_Library.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_MOSI 9 // D1
-#define OLED_CLK 10 // D0
-#define OLED_DC 11 // DC
-#define OLED_CS 12 // CS
-#define OLED_RESET 13 // RES
+// 4-pin
+#define OLED_RESET 4 // RES
+#define SCREEN_ADDRESS 0x3C // 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+// 7-pin
+// #define OLED_MOSI 2 // D1
+// #define OLED_CLK 3 // D0
+// #define OLED_DC 4 // DC
+// #define OLED_CS 5 // CS
+// #define OLED_RESET 6 // RES
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI,
+OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+SGP30 sgp30;
+SGP30ERR error;
 
 void setup() {
-  Serial.println("Setting up sketch");
+  // Two LEDs
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 
-  int eco2 = readSGP30(0);
-  int tvoc = readSGP30(1);
-  bool isGoodAir = isGoodAirQuality(eco2, tvoc);
+  Serial.begin(9600);
+  Wire.begin();
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  // if (!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
 
-  print(eco2, tvoc, isGoodAir);
-  blink(isGoodAir);
+  if (sgp30.begin() == false) {
+    Serial.println("No SGP30 Detected. Check connections.");
+    printWarning('A');
+    while (1);
+  }
+  sgp30.initAirQuality();
+}
+
+int prevEco2 = 0;
+int prevTvoc = 0;
+
+void loop() {
+  error = sgp30.measureAirQuality();
+  if (error == SGP30_SUCCESS) {
+    int eco2 = sgp30.CO2;
+    int tvoc = sgp30.TVOC;
+
+    if (!isBaseValues(eco2, tvoc)) {
+      if (prevEco2 != eco2 || prevTvoc != tvoc) {
+        bool isGoodAir = isGoodAirQuality(eco2, tvoc);
+        printSuccess(eco2, tvoc, isGoodAir);
+        blink(isGoodAir);
+        prevEco2 = eco2;
+        prevTvoc = tvoc;
+      }
+      delay(9000);
+    } else {
+      printWarning('B');
+      blink(true);
+    }
+  }
+  delay(1000);
 }
 
 void blink(bool isGoodAir) {
-  if (!isGoodAir) {
+  if (isGoodAir) {
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+  } else {
     digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
   }
-}
-
-void loop() {}
-
-int readSGP30(int mode) {
-  if (mode == 0) {
-    return 701;
-  }
-  if (mode == 1) {
-    return 320;
-  }
-  return 0;
-}
-
-bool isGoodAirQuality(int eco2, int tvoc) {
-  return isGoodECO2(eco2) && isGoodTVOC(tvoc);
-}
-
-bool isGoodECO2(int eco2) {
-  return 0 < eco2 && eco2 < 700;
-}
-
-bool isGoodTVOC(int tvoc) {
-  return 0 < tvoc && tvoc < 700;
-}
-
-void iAmNotFeelingWell() {
-
 }
 
 void printFirstRow(unsigned char lang, bool isGoodAir) {
@@ -93,30 +114,20 @@ void printFirstRow(unsigned char lang, bool isGoodAir) {
         display.println("Gute Luft!");
       } else {
         display.println("Stossluft!");
-      } 
-      break;
-    case 'C':
-      // 
-      break;
-    case 'D':
-      // 
+      }
       break;
     default:
-      iAmNotFeelingWell();
+      display.println("Error");
       break;
   }
 }
 
-void print(int eco2, int tvoc, bool isGoodAir) {
-
-  display.begin(SSD1306_SWITCHCAPVCC); 
-  display.clearDisplay();
-  display.display();     
+void printSuccess(int eco2, int tvoc, bool isGoodAir) {
   display.setTextColor(WHITE);
 
-  printFirstRow('B', isGoodAir);
+  printFirstRow('A', isGoodAir);
   display.drawFastHLine(0, 24, 128, WHITE);
- 
+
   display.setTextSize(2);
   display.setCursor(4, 30);
   display.print("VOC:");
@@ -129,5 +140,49 @@ void print(int eco2, int tvoc, bool isGoodAir) {
   display.print(eco2);
   display.setTextSize(1);
   display.println("ppm");
+
   display.display();
+  display.clearDisplay();
+}
+
+void printWarning(unsigned char code) {
+  display.setTextColor(WHITE);
+  display.setCursor(4, 4);
+  display.setTextSize(1);
+
+  switch (code) {
+    case 'A':
+      display.println("Sensor not found.");
+      display.println("Trying again ...");
+      break;
+    case 'B':
+      display.println("Sensor calibrating.");
+      break;
+    case 'C':
+      display.println("Sensor reading error.");
+      display.println("Trying again ...");
+      break;
+    default:
+      display.println("Unknown warning");
+      break;
+  }
+
+  display.display();
+  display.clearDisplay();
+}
+
+bool isGoodAirQuality(int eco2, int tvoc) {
+  return isGoodECO2(eco2) && isGoodTVOC(tvoc);
+}
+
+bool isGoodECO2(int eco2) {
+  return 0 <= eco2 && eco2 < 750;
+}
+
+bool isGoodTVOC(int tvoc) {
+  return 0 <= tvoc && tvoc < 150;
+}
+
+bool isBaseValues(int eco2, int tvoc) {
+  return eco2 == 400 && tvoc == 0;
 }
